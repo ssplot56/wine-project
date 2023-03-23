1df000728d8aa04d1f27599f151d32525525ed12
ssplot56 <ssplot56@gmail.com>
2023-03-23 19:10:50 +0200

    initial commit

---
+++ src/main/java/com/project/wineproject/WineProjectApplication.java
@@ -0,0 +1,13 @@
+package com.project.wineproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WineProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WineProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/wineproject/WineProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.wineproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WineProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

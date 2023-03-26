ba56dc2a0a5c96b581e35cb84e5be57991598274
djmolot <djmolot@gmail.com>
2023-03-26 18:59:02 +0200

    added annotations over fiels of enum type

--- src/main/java/com/project/wineproject/model/Product.java
+++ src/main/java/com/project/wineproject/model/Product.java
@@ -1,6 +1,11 @@
 package com.project.wineproject.model;
 
+import com.project.wineproject.model.enums.ProductColor;
+import com.project.wineproject.model.enums.ProductType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -19,8 +24,12 @@ public class Product {
 
     private String name;
 
+    @Column(length = 20)
+    @Enumerated(EnumType.STRING)
     private ProductColor productColor;
 
+    @Column(length = 20)
+    @Enumerated(EnumType.STRING)
     private ProductType productType;
 
     @ManyToOne

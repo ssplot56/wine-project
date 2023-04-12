cdd95bc0d05f0a2f92014de68c96559271ec7a22
djmolot <djmolot@gmail.com>
2023-04-12 08:13:20 +0200

    changed relations between classes Product & Dish

--- src/main/java/com/project/wineshop/controller/ProductController.java
+++ src/main/java/com/project/wineshop/controller/ProductController.java
@@ -64,7 +64,7 @@ public class ProductController {
 
     //http://localhost:8080/wine-shop/products?size=6&page=0&sortBy=price
     //http://localhost:8080/wine-shop/products?sortBy=price:DESC;name:ASC&page=0
-    @GetMapping
+    @GetMapping("/pagedsorted")
     public ResponseEntity<List<ProductResponseDto>> getAllProducts(@RequestParam (defaultValue = "20") Integer size,
                                                                    @RequestParam (defaultValue = "0") Integer page,
                                                                    @RequestParam (defaultValue = "id") String sortBy) {
@@ -81,6 +81,24 @@ public class ProductController {
     @GetMapping("/filtered")
     public ResponseEntity<List<ProductResponseDto>> getFilteredProducts(@RequestParam Map<String, String> params) {
         List<ProductResponseDto> responseDtos = productService.findAll(params)
+                .stream()
+                .map(productMapper::mapToDto)
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(responseDtos);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductResponseDto>> getProducts(
+            @RequestParam(required = false) String type,
+            @RequestParam(required = false) String color,
+            @RequestParam(required = false) String event,
+            @RequestParam(required = false) String dishes,
+            @RequestParam(defaultValue = "id:ASC") String sortBy,
+            @RequestParam(defaultValue = "0") Integer page,
+            @RequestParam(defaultValue = "10") Integer size) {
+
+        List<ProductResponseDto> responseDtos = productService.getProducts(
+                type, color, event, dishes, sortBy, page, size)
                 .stream()
                 .map(productMapper::mapToDto)
                 .collect(Collectors.toList());
@@ -109,7 +127,6 @@ public class ProductController {
 
     @GetMapping("/inject")
     public String inject(@RequestParam(defaultValue = "100") Integer count) {
-        injectDishes();
 
         injectWinePink1();
         injectWinePink2();
@@ -133,33 +150,6 @@ public class ProductController {
         injectWineWhite6();
 
         return "Created 18 products";
-    }
-
-    private void injectDishes() {
-        List<String> names = new ArrayList<>();
-        names.add("Pasta");
-        names.add("Meat");
-        names.add("Cheese");
-        names.add("Seafood");
-        names.add("Rice");
-        names.add("Fish");
-        names.add("Salads");
-        names.add("Chicken");
-        names.add("Barbeques");
-        names.add("Sausages");
-        names.add("Poultry");
-        names.add("Risottos");
-        names.add("Strong dishes");
-        names.add("Dishes with personality");
-        names.add("Smoked dishes");
-        names.add("Vegetables");
-        names.add("Foie");
-
-        for (String name : names) {
-            Dish dish = new Dish();
-            dish.setName(name);
-            dishService.save(dish);
-        }
     }
 
     private void injectWinePink1() {
--- src/main/java/com/project/wineshop/model/Product.java
+++ src/main/java/com/project/wineshop/model/Product.java
@@ -1,13 +1,16 @@
 package com.project.wineshop.model;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -40,9 +43,6 @@ public class Product {//equals & hashcode Lombok
     private String pairing;
 
     @ManyToMany
-    @JoinTable(name = "products_dishes",
-            joinColumns = @JoinColumn(name = "product_id"),
-            inverseJoinColumns = @JoinColumn(name = "dish_id"))
     private Set<Dish> dishes;
 
     @Column(length = 4)

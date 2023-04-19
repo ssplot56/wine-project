090f66825eb672272ad5c2ee82280e4cd7e9a356
ssplot56 <ssplot56@gmail.com>
2023-04-19 16:21:25 +0300

    switched return value from String to UserResponseDto

--- src/main/java/com/project/wineshop/controller/AuthenticationController.java
+++ src/main/java/com/project/wineshop/controller/AuthenticationController.java
@@ -3,7 +3,10 @@ package com.project.wineshop.controller;
 import com.project.wineshop.dto.request.user.UserLoginDto;
 import com.project.wineshop.dto.request.user.UserRegisterDto;
 import com.project.wineshop.dto.response.JwtAuthResponse;
+import com.project.wineshop.dto.response.UserResponseDto;
+import com.project.wineshop.model.User;
 import com.project.wineshop.service.AuthenticationService;
+import com.project.wineshop.service.mapper.impl.UserMapper;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,15 +21,19 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "*")
 public class AuthenticationController {
     private final AuthenticationService authenticationService;
+    private final UserMapper userMapper;
 
-    public AuthenticationController(AuthenticationService authenticationService) {
+    public AuthenticationController(AuthenticationService authenticationService,
+                                    UserMapper userMapper) {
         this.authenticationService = authenticationService;
+        this.userMapper = userMapper;
     }
 
     @PostMapping("/sign-up")
-    public ResponseEntity<String> register(@Valid @RequestBody UserRegisterDto userRegisterDto) {
-        authenticationService.register(userRegisterDto);
-        return new ResponseEntity<>("The user has been registered.", HttpStatus.CREATED);
+    public ResponseEntity<UserResponseDto> register(
+            @Valid @RequestBody UserRegisterDto userRegisterDto) {
+        User user = authenticationService.register(userRegisterDto);
+        return new ResponseEntity<>(userMapper.mapToDto(user), HttpStatus.CREATED);
     }
 
     @PostMapping("/log-in")

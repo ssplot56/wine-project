4735510e6aeb7cd2ff7901fc3879d49aa5a7a0b3
djmolot <djmolot@gmail.com>
2023-04-25 09:49:42 +0200

    added method joinAllSortBy() in class ProductServiceImpl

--- src/main/java/com/project/wineshop/service/impl/ProductServiceImpl.java
+++ src/main/java/com/project/wineshop/service/impl/ProductServiceImpl.java
@@ -5,8 +5,12 @@ import com.project.wineshop.repository.ProductRepository;
 import com.project.wineshop.repository.specification.SpecificationManager;
 import com.project.wineshop.service.ProductService;
 import com.project.wineshop.utility.PageRequestFormer;
+
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
@@ -37,26 +41,27 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public List<Product> findAll(Map<String, String> params) {
+        Map<String, String> paramsProcessed = joinAllSortBy(params);
         Integer page = 0;
-        if (params.containsKey("page")) {
-            page = Integer.valueOf(params.get("page"));
-            params.remove("page");
+        if (paramsProcessed.containsKey("page")) {
+            page = Integer.valueOf(paramsProcessed.get("page"));
+            paramsProcessed.remove("page");
         }
         Integer size = 6;
-        if (params.containsKey("size")) {
-            size = Integer.valueOf(params.get("size"));
-            params.remove("size");
+        if (paramsProcessed.containsKey("size")) {
+            size = Integer.valueOf(paramsProcessed.get("size"));
+            paramsProcessed.remove("size");
         }
         String sortBy = "id:ASC";
-        if (params.containsKey("sortBy")) {
-            sortBy = params.get("sortBy");
-            params.remove("sortBy");
+        if (paramsProcessed.containsKey("sortBy")) {
+            sortBy = paramsProcessed.get("sortBy");
+            paramsProcessed.remove("sortBy");
         }
         PageRequest pageRequest = PageRequestFormer.formPageRequest(page, size, sortBy);
 
         Specification<Product> specification = null;
-        if (!params.isEmpty()) {
-            for (Map.Entry<String, String> entry : params.entrySet()) {
+        if (!paramsProcessed.isEmpty()) {
+            for (Map.Entry<String, String> entry : paramsProcessed.entrySet()) {
                 Specification<Product> sp = productSpecificationManager.get(entry.getKey(),
                         entry.getValue().split(","));
                 specification = specification == null ? Specification.where(sp)
@@ -79,5 +84,29 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public void deleteById(Long id) {
         productRepository.deleteById(id);
+    }
+
+    //костиль спеціально на прохання фронтенду, щоб він міг відправляти запити так як йому зручно
+    //це звичайно bad practice, але доводиться якось знаходити спільну мову з фронтендом
+    private Map<String, String> joinAllSortBy(Map<String, String> params) {
+        Map<String, String> result = new HashMap<>();
+        Map<String, String> sortParams = new HashMap<>();
+
+        for (Map.Entry<String, String> entry : params.entrySet()) {
+            if (entry.getKey().contains("sortBy")) {
+                sortParams.put(entry.getKey(), entry.getValue());
+            } else {
+                result.put(entry.getKey(), entry.getValue());
+            }
+        }
+
+        if (!sortParams.isEmpty()) {
+            String sortBy = sortParams.entrySet().stream()
+                    .map(e -> e.getKey().replace("sortBy", "").toLowerCase() + ":" + e.getValue())
+                    .collect(Collectors.joining(";"));
+            result.put("sortBy", sortBy);
+        }
+
+        return result;
     }
 }
